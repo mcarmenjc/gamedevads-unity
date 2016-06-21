a0f46ca92bfbc2a532c456f187121d23c2d01a55
Paul Maidment <paul.maidment@affiliatewindow.com>
2016-06-21 15:08:22 +0100

    Asset bundle with scenes, loader demo

--- AdDimensionAdvertiser/Assets/Editor/CreateAssetBundles.cs
+++ AdDimensionAdvertiser/Assets/Editor/CreateAssetBundles.cs
@@ -7,4 +7,11 @@ public class CreateAssetBundles
 	{
 		BuildPipeline.BuildAssetBundles ("Assets/Build/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneLinuxUniversal);
 	}
+
+	[MenuItem ("Build/BuildWebplayerStreamed")]
+	public static void MyBuild(){
+		string[] levels = new string[] {"Assets/Scenes/advert.unity"};
+		BuildPipeline.BuildStreamedSceneAssetBundle( levels, "Assets/Build/advert.unity3d", BuildTarget.StandaloneLinuxUniversal); 
+	}
+
 }
