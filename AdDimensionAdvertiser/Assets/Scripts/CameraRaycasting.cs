88a6ba07c63b269d9bc2df4abd6b1430749feb74
MCarmen Jimenez Campos <mcarmen.jimenez.campos@gmail.com>
2016-06-21 16:37:38 +0200

    raycasting

---
+++ AdDimensionAdvertiser/Assets/Scripts/CameraRaycasting.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRaycasting : MonoBehaviour {
+
+	private Transform _camera;
+
+	private void Start()
+	{
+		_camera = GetComponent<Transform> ();
+	}
+
+	private void Update()
+	{
+		EyeRaycast();
+	}
+		
+	private void EyeRaycast()
+	{
+		Debug.DrawRay(_camera.position, _camera.forward * 100, Color.blue, 1);
+		RaycastHit hit;
+		// Create a ray from the transform position along the transform's z-axis
+		Ray ray = new Ray(_camera.position, _camera.forward);
+		if (Physics.Raycast (ray, out hit)) 
+		{
+			Debug.Log ("HIT 3D");
+			GameObject selectedObject = hit.transform.gameObject;
+		}
+	}
+}
